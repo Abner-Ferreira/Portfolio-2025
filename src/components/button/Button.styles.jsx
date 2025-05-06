95118ecd67dd9fcc6dddecf46c62cf223b690c3a
Abner-Ferreira <abnerferr2015@gmail.com>
2025-05-06 16:14:51 -0300

    Estilizacao em telas maiores do Botao

--- src/components/button/Button.styles.jsx
+++ src/components/button/Button.styles.jsx
@@ -1,18 +1,23 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const Btn = styled.button `
+export const Btn = styled.button`
   width: 40%;
   height: 40px;
   margin-top: 20px;
-  background-color: ${({theme}) => theme.highlights};
+  background-color: ${({ theme }) => theme.highlights};
   border: none;
   border-radius: 20px;
   display: flex;
   align-items: center;
   justify-content: center;
-  `
 
-export const Link = styled.a `
+  /* 4K e acima */
+  @media (min-width: 1920px) {
+    align-self: self-start;
+  }
+`
+
+export const Link = styled.a`
   font-size: 20px;
   height: 100%;
   width: 100%;
@@ -20,5 +25,5 @@ export const Link = styled.a `
   align-items: center;
   justify-content: center;
   text-decoration: none;
-  color: ${({theme}) => theme.mainText}; 
+  color: ${({ theme }) => theme.mainText};
 `
