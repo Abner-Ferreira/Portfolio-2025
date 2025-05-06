4f002d787492596f54092409ad628ca06566d271
Abner-Ferreira <abnerferr2015@gmail.com>
2025-05-06 17:20:58 -0300

    Estlizacao do portfolio feita para telas maiores

--- src/pages/portfolio/Portfolio.styles.jsx
+++ src/pages/portfolio/Portfolio.styles.jsx
@@ -12,6 +12,21 @@ export const Titulo = styled.h1`
   font-size: 40px;
   text-align: center;
   color: ${({ theme }) => theme.mainText};
+
+  /* Tablets (retrato) */
+  @media (500px <= width <= 850px) {
+    font-size: 46px;
+  }
+
+  /* Tablets (paisagem) */
+  @media (851px <= width <= 1080px) {
+    font-size: 48px;
+  }
+
+  /* 4K e acima */
+  @media (min-width: 1920px) {
+    font-size: 54px;
+  }
 `
 
 export const Linha = styled.div`
@@ -19,4 +34,19 @@ export const Linha = styled.div`
   height: 5px;
   margin-top: 10px;
   background-color: ${({ theme }) => theme.highlights};
+
+  /* Tablets (retrato) */
+  @media (500px <= width <= 850px) {
+    width: 14%;
+  }
+
+  /* Tablets (paisagem) */
+  @media (851px <= width <= 1080px) {
+    width: 10%;
+  }
+
+  /* 4K e acima */
+  @media (min-width: 1920px) {
+    width: 6%;
+  }
 `
